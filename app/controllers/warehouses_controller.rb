04b338beb26b6eb62d940819cd74384032acae30
andrea-finocchiaro <andrea.amnaya@gmail.com>
2022-09-19 02:11:01 -0300

    Cadastrando galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,17 @@ class WarehousesController < ApplicationController
     id = params[:id]
     @warehouse = Warehouse.find(id)
   end
+
+  def new
+  end
+
+  def create
+    #receber os dados enviados
+    # criar um novo X no bd
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area) #Strong Parameters
+    w = Warehouse.new(warehouse_params)
+    w.save()
+    #redirecionar para root
+    redirect_to root_path
+  end
 end
