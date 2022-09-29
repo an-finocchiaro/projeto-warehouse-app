cb96e3f24b253a5279a26de54cddcc73ebaf971b
andrea-finocchiaro <andrea.amnaya@gmail.com>
2022-09-29 01:56:01 -0300

    listar product_models de fornecedor em detalhes

--- spec/system/suppliers/user_view_supplier_details_spec.rb
+++ spec/system/suppliers/user_view_supplier_details_spec.rb
@@ -28,4 +28,22 @@ describe 'Usuário vê detalhes de um fornecedor' do
     #Assert
     expect(current_path).to eql(suppliers_path)
   end
+
+  it 'e vê seus Modelos de Produtos' do
+    #Arrange
+    supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos Ltda', registration_number: '1234567890123', full_address: 'Av. Naçoes Unidas, 1000', city: 'Sao Paulo', state: 'SP', email: 'sac@samsung.com.br', phone_number: '(11) 5501-3300')
+    product_model = ProductModel.create!(name: 'TV 32', weight: 8000, width:70, height:45, depth:10, sku:'TV32-SAMS-VTD-547236', supplier: supplier)
+    other_product_model = ProductModel.create!(name: 'Soundbar 7.1 Surround', weight: 3000, width:80, height:15, depth:20, sku:'SOU71-SAMSU-NOIZ7743', supplier: supplier)
+    #Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Samsung'
+    #Assert
+    expect(page).to have_content 'Soundbar 7.1 Surround'
+    expect(page).to have_content '8000'
+    expect(page).to have_content '80'
+    expect(page).to have_content '45'
+    expect(page).to have_content '20'
+    expect(page).to have_content 'TV32-SAMS-VTD-547236'
+  end
 end
