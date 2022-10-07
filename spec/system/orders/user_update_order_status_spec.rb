692a1aba0aa63d585ca01c86117e86d5ade8daf3
andrea-finocchiaro <andrea.amnaya@gmail.com>
2022-10-07 18:44:53 -0300

    Adicionar itens ao estoque

--- spec/system/orders/user_update_order_status_spec.rb
+++ spec/system/orders/user_update_order_status_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 describe 'Usuário informa novo status de pedido' do
   it 'e pedido foi entregue' do
     #Arrange
-    ana = User.create!(name: 'Ana', email: 'ana@email.com', password: '11223344')
+    user = User.create!(name: 'Ana', email: 'ana@email.com', password: '11223344')
 
     warehouse = Warehouse.create!(name: 'Galpão Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000, address: 'Av. do Porto, 1000', cep: '20000-000', description: 'Galpão do Rio')
  
     supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME',
     registration_number: '4344721600010', email: 'contato@acme.com', full_address: 'Av. das Palmas, 100', city: 'Bauru', state: 'SP', phone_number: '(15) 4330-1000')
 
-    order = Order.create!(user: ana, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 2.days.from_now, status: :pending)
+    product = ProductModel.create!(name: 'Cadeira Gamer', weight: 5, width: 70, height: 100, depth: 75, supplier: supplier, sku: 'ABCDE-12456-112211-A')
+
+    order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 2.days.from_now, status: :pending)
+
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
     #Act
-    login_as(ana)
+    login_as(user)
     visit root_path
     click_on 'Meus Pedidos'
     click_on order.code
@@ -22,20 +26,27 @@ describe 'Usuário informa novo status de pedido' do
     expect(page).to have_content 'Situação do Pedido: Entregue'
     expect(page).not_to have_button 'Marcar como CANCELADO'
     expect(page).not_to have_button 'Marcar como ENTREGUE'
+    expect(StockProduct.count).to eq 5
+    estoque = StockProduct.where(product_model: product, warehouse: warehouse).count
+    expect(estoque).to eq 5
   end
 
   it 'e pedido foi cancelado' do
     #Arrange
-    ana = User.create!(name: 'Ana', email: 'ana@email.com', password: '11223344')
+    user = User.create!(name: 'Ana', email: 'ana@email.com', password: '11223344')
 
     warehouse = Warehouse.create!(name: 'Galpão Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000, address: 'Av. do Porto, 1000', cep: '20000-000', description: 'Galpão do Rio')
  
     supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME',
     registration_number: '4344721600010', email: 'contato@acme.com', full_address: 'Av. das Palmas, 100', city: 'Bauru', state: 'SP', phone_number: '(15) 4330-1000')
 
-    order = Order.create!(user: ana, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 2.days.from_now, status: :pending)
+    product = ProductModel.create!(name: 'Cadeira Gamer', weight: 5, width: 70, height: 100, depth: 75, supplier: supplier, sku: 'ABCDE-12456-112211-A')
+
+    order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 2.days.from_now, status: :pending)
+
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
     #Act
-    login_as(ana)
+    login_as(user)
     visit root_path
     click_on 'Meus Pedidos'
     click_on order.code
@@ -43,5 +54,6 @@ describe 'Usuário informa novo status de pedido' do
     #Assert
     expect(current_path).to eq order_path(order.id)
     expect(page).to have_content 'Situação do Pedido: Cancelado'
+    expect(StockProduct.count).to eq 0
   end
 end
